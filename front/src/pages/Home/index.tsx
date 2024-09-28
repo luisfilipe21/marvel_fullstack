719b7151e26a8f3a83fbddf9d628a5e6e64d1821
Luis Filipe Guimarães Costa <luisfilipegc@hotmail.com>
2024-09-28 17:24:55 -0300

    feat: limpeza do código

--- front/src/pages/Home/index.tsx
+++ front/src/pages/Home/index.tsx
@@ -7,16 +7,6 @@ export const Home = () => {
         <>
             <Header />
             <main className=" h-auto flex flex-col items-center justify-center">
-
-                {/* {comicId && comicId.digitalId && comicId.map(images => {
-                    <div>
-                        <h1>aqui</h1>
-                        <a href={`${images.path}.${images.extension}`} target="_blank">Link </a>
-                    </div>
-                })
-
-                } */}
-
                 <HeroList />
             </main>
         </>
